102a184993af6cea99393674d913dbef9514c2ee
FranciscoGontijo <franciscoacmg@gmail.com>
2024-07-11 18:02:01 -0300

    Adding multiples emais to participants

--- src/routes/create-trip.ts
+++ src/routes/create-trip.ts
@@ -16,10 +16,11 @@ export async function createTrip(app: FastifyInstance) {
                 ends_at: z.coerce.date(),
                 owner_name: z.string(),
                 owner_email: z.string().email(),
+                emails_to_invite: z.array(z.string().email())
             })
         }
     }, async (request) => {
-        const { destination, ends_at, starts_at, owner_email, owner_name } = request.body
+        const { destination, ends_at, starts_at, owner_email, owner_name, emails_to_invite } = request.body
 
         if (dayjs(starts_at).isBefore(new Date())) {
             throw new Error('Invalid trip start date');
@@ -35,11 +36,19 @@ export async function createTrip(app: FastifyInstance) {
                 starts_at,
                 ends_at,
                 participants: {
-                    create: {
-                        name: owner_name,
-                        email: owner_email,
-                        is_owner: true,
-                        is_confirmed: true
+                    createMany: {
+                        data: [
+                            {
+                                name: owner_name,
+                                email: owner_email,
+                                is_owner: true,
+                                is_confirmed: true
+                            },
+                            // Add all emails passed as emails to invite to participants
+                            ...emails_to_invite.map(email => {
+                                return { email }
+                            })
+                        ],
                     }
                 }
             }
@@ -56,8 +65,8 @@ export async function createTrip(app: FastifyInstance) {
                 name: owner_name,
                 address: owner_email
             },
-            subject: 'Testando envio de email',
-            html: `<p>Test do envio de email</p>`
+            subject: `Confirme sua viagem para ${destination}`,
+            html: ``.trim()
         });
 
         console.log(nodemailer.getTestMessageUrl(message))
