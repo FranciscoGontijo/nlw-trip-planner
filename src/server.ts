495bb9b3f3a5922ae29557c4b70be5796df3a6b2
FranciscoGontijo <franciscoacmg@gmail.com>
2024-07-13 11:34:48 -0300

    Register routes to app

--- src/server.ts
+++ src/server.ts
@@ -8,6 +8,11 @@ import { createActivity } from './routes/create-activity';
 import { getActivities } from './routes/get-activities';
 import { createLink } from './routes/create-link';
 import { getLinks } from './routes/get-links';
+import { getParticipants } from './routes/get-participants';
+import { createInvite } from './routes/create-invite';
+import { updateTrip } from './routes/update-trip';
+import { getTripDetails } from './routes/get-trip-details';
+import { getParticipant } from './routes/get-participant';
 
 const app = fastify();
 
@@ -18,6 +23,7 @@ app.register(cors, {
 app.setValidatorCompiler(validatorCompiler);
 app.setSerializerCompiler(serializerCompiler);
 
+//Register routes
 app.register(createTrip);
 app.register(confirmTrip);
 app.register(confirmParticipant);
@@ -25,6 +31,12 @@ app.register(createActivity);
 app.register(getActivities);
 app.register(createLink);
 app.register(getLinks);
+app.register(getParticipants);
+app.register(createInvite);
+app.register(updateTrip);
+app.register(getTripDetails);
+app.register(getParticipant);
+
 
 app.listen({ port: 3333 }).then(() => {
     console.log('Server running');
